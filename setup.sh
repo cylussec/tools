711dd090737456387234e72339d5645a8f739888
Brian Seel <brian.seel@gmail.com>
2014-06-30 16:55:00 -0700

    added ssh keys, git config and ignore, and updated the setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,13 @@
-apt-get update
-apt-get -y upgrade
-apt-get -y dist-upgrade
-apt-get install terminator
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get install terminator
+sudo apt-get install open-vm-tools
+cp git/* ~
+unzip ssh.zip -d
+eval `ssh-agent -s`
+ssh-add ~/.ssh/id_rsa
+  
+
+echo "# Aliases" >> ~/.bashrc
+echo "alias update='sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade'" >> ~/.bashrc
