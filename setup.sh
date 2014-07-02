423790f92aa19c3b5de95cf6ae6d106c03b1d1c7
Brian Seel <brian.seel@gmail.com>
2014-07-02 14:04:25 -0400

    added .vimrc to folder and setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,18 @@
+#get everything installed and updated
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
 sudo apt-get install terminator
 sudo apt-get install open-vm-tools
-cp git/* ~
-unzip ssh.zip -d
-eval `ssh-agent -s`
-ssh-add ~/.ssh/id_rsa
-  
 
+#copy everything to where it needs to be
+cp git/* ~              #git configs
+unzip ssh.zip -d        #ssh keys (will ask for password
+eval `ssh-agent -s`     
+ssh-add ~/.ssh/id_rsa
+cp .vimrc ~
+
+#add aliases to bashrc
 echo "# Aliases" >> ~/.bashrc
 echo "alias update='sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade'" >> ~/.bashrc
+echo "eval `ssh-agent -s`"
