45b93a2c054d19d6107108270192dce9807899a1
Brian Seel <brian.seel@gmail.com>
2014-07-06 18:27:19 -0400

    tested script and added a few things

--- setup.sh
+++ setup.sh
@@ -1,13 +1,18 @@
-sudo apt-get update
+sdo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
-sudo apt-get install terminator
-sudo apt-get install open-vm-tools
-cp git/* ~
-unzip ssh.zip -d
+sudo apt-get -y install terminator
+sudo apt-get -y install open-vm-tools
+sudo apt-get -y install vim
+sudo apt-get -y install dropbox
+sudo apt-get -y install spyder
+cp git/.* ~
+unzip ssh.zip -d ~
 eval `ssh-agent -s`
 ssh-add ~/.ssh/id_rsa
   
 
 echo "# Aliases" >> ~/.bashrc
 echo "alias update='sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade'" >> ~/.bashrc
+echo "" >> ~/.bashrc
+echo "eval `ssh-agent -s'" >> ~/.bashrc
