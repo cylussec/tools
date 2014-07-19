7733f80bead67479709e1db813d9931b6af68e0d
Brian Seel <brian.seel@gmail.com>
2014-07-19 12:23:04 -0400

    switched to keychain instead of trying to do it with ssh-agent... also cleaned up the apt-get installswq

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
-sudo apt-get -y install terminator
-sudo apt-get -y install open-vm-tools
-sudo apt-get -y install vim
-sudo apt-get -y install dropbox
-sudo apt-get -y install spyder
+sudo apt-get -y install terminator open-vm-tools vim dropbox keychain spyder
+
 cp git/.* ~
 unzip ssh.zip -d ~
 eval `ssh-agent -s`
@@ -17,3 +14,8 @@ echo "# Aliases" >> ~/.bashrc
 echo "alias update='sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade'" >> ~/.bashrc
 echo "" >> ~/.bashrc
 echo "eval `ssh-agent -s`" >> ~/.bashrc
+
+#This sets up the keychain
+/usr/bin/keychain $HOME/.ssh/id_rsa
+source $HOME/.keychain/$HOSTNAME-sh
+
